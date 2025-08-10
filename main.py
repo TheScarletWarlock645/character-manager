8eb1710819bb730e4af1aee7cd02aea26d0843e4
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-08-10 03:57:42 -0400

    added settings.json and updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import rich
 from rich import print as rprint
 
@@ -6,7 +7,7 @@ try:
     print()
     print("Welcome to Character Manager!")
     print()
-    startActions = ["Make new character", "Edit existing character","Export character sheet"]
+    startActions = ["Make new character", "Edit existing character","Export character sheet", "Make or change character library"]
     for i, item in enumerate(startActions, 1):
         print(f"{i}. {item}")
 
@@ -15,15 +16,43 @@ try:
     if startChoice == 1:
         createChoice = int(input("Guided or non guided character creation? (1, 2) "))
 
+        statList = ["Name", "Class and level", "Race", "Background", "Alignment", "XP", "Prof bonus", "Ability scores", "Ability profs", "Skill profs",
+        "Other profs and languages", "Armour class", "Initiative", "Speed", "HP", "Hit dice", "Money", "Equipment", "Features and traits", "Attacks and spellcasting"]
+        statListList = len(statList) // 2
+
         if createChoice == 1:
+            print()
             print("You chose: guided character creation")
+            print()
+
         elif createChoice == 2:
+            print()
             print("You chose: non-guided character creation")
+            print("Choose a stat to edit:")
+            print()
+            for i in range(statListList):
+                left = f"{i+1}. {statList[i]}"
+                right = f"{i+1+statListList}. {statList[i+statListList]}"
+                print(f"{left:<30} {right}")
+
 
     elif startChoice == 2:
         print()
     elif startChoice == 3:
         print()
+    elif startChoice == 4:
+        print()
+        settingsChange = int(input("Would you like to change the character library path or make a new directory? (1, 2) "))
+        print()
+        
+        if settingsChange == 1:
+            chCarLib = input("Enter new path for ")
+            newPath = {
+                "CharLibPath": f"{chCarLib}"
+            }
+            with open('settings.json', 'w') as f:
+                json.dump(newPath, f)
+
     else:
         print()
         rprint("[bold red]ERROR: Please enter a valid number.[/bold red]")
