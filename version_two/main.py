22890093129982d708c42376d1ed9b81843b7982
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-09-03 18:21:28 -0400

    Made version two  and improved everything

---
+++ version_two/main.py
@@ -0,0 +1,47 @@
+import json
+import rich
+import os
+from rich import print as rprint
+from rich.prompt import Prompt
+
+try:
+    def loadfile(file):
+        try:
+            with open(f"{file}", 'r', encoding="utf-8") as f:
+                return f.read()
+        except:
+            return "ERROR"
+
+    start_actions = ["Make new character", "Edit existing characters", "Export character", "Edit settings"]
+
+    print(f"\n{loadfile("logo.txt")}\n")
+    rprint("Welcome to Character Manager!")
+    for i, name in enumerate(start_actions, 1):
+        print(f"{i}. {name}")
+    print()
+
+    start_choice = Prompt.ask("Choice: ")
+    print()
+
+    if start_choice == "1":
+        pass
+    elif start_choice == "2":
+        pass
+    elif start_choice == "3":
+        pass
+    elif start_choice == "4":
+        settings = ["Character library path"]
+        for i, name in enumerate(settings, 1):
+            print(f"{i}. {name}")
+        print()
+        settings_choice = input("Choice: ")
+        print()
+
+        if settings_choice == "1":
+            with open("settings.json", "w") as f:
+                json.dump(new_path, f)
+        else:
+            os.system(f"python3 {__file__}")
+        
+except KeyboardInterrupt:
+    exit("\nGoodbye!")
